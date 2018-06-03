373cb11f47679858dddb0e57749b8b54360c65a0
ksekimoto <jbh00623@nifty.com>
2018-06-03 21:14:50 +0900

    Updated from SPACE to TAB

--- firmware_release/wrbb_mruby/sJpeg.h
+++ firmware_release/wrbb_mruby/sJpeg.h
@@ -13,45 +13,45 @@
 class sJpeg
 {
 private:
-    pjpeg_image_info_t image_info;
-    int is_available;
-    int mcu_x;
-    int mcu_y;
-    uint32_t row_pitch;
-    uint32_t row_blocks_per_mcu;
+	pjpeg_image_info_t image_info;
+	int is_available;
+	int mcu_x;
+	int mcu_y;
+	uint32_t row_pitch;
+	uint32_t row_blocks_per_mcu;
 	uint32_t col_blocks_per_mcu;
-    uint8_t reduce;
-    int decode_mcu(void);
+	uint8_t reduce;
+	int decode_mcu(void);
 public:
-    uint8_t *pImage;
+	uint8_t *pImage;
 
-    int err;
-    int decoded_width;
-    int decoded_height;
-    int comps;
-    int MCUSPerRow;
-    int MCUSPerCol;
-    pjpeg_scan_type_t scanType;
-    int MCUx;
-    int MCUy;
-    
-    sJpeg();
-    ~sJpeg();
-    int decode(char* pFilename, unsigned char pReduce);
-    int available(void);
-    int read(void);
-    inline int width() {
-    	return (int)image_info.m_width;
-    }
-    inline int height() {
-    	return (int)image_info.m_height;
-    }
-    inline int MCUWidth() {
-    	return (int)image_info.m_MCUWidth;
-    }
-    inline int MCUHeight() {
-    	return (int)image_info.m_MCUHeight;
-    }
+	int err;
+	int decoded_width;
+	int decoded_height;
+	int comps;
+	int MCUSPerRow;
+	int MCUSPerCol;
+	pjpeg_scan_type_t scanType;
+	int MCUx;
+	int MCUy;
+
+	sJpeg();
+	~sJpeg();
+	int decode(char* pFilename, unsigned char pReduce);
+	int available(void);
+	int read(void);
+	inline int width() {
+		return (int)image_info.m_width;
+	}
+	inline int height() {
+		return (int)image_info.m_height;
+	}
+	inline int MCUWidth() {
+		return (int)image_info.m_MCUWidth;
+	}
+	inline int MCUHeight() {
+		return (int)image_info.m_MCUHeight;
+	}
 };
 
 extern sJpeg jpeg;
